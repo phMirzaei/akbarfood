bdd230909ea5758631c5653d3d5dd08856419f19
phatemeh <phz.mirzaee@gmail.com>
2026-06-04 21:04:45 +0330

    Adding use Illuminate\Http\Request to restaurantController

--- app/Http/Controllers/Restaurants/RestaurantController.php
+++ app/Http/Controllers/Restaurants/RestaurantController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Restaurant\RegisterRequest;
 use App\Models\Restaurant\Restaurant;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
 {
