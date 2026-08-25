3c7421c09b928fdbbb32f6d1c417f5fde2afd258
phatemeh <phz.mirzaee@gmail.com>
2026-08-25 11:56:22 +0330

    routes in api organized

--- routes/api.php
+++ routes/api.php
@@ -11,40 +11,47 @@ use App\Http\Controllers\Payment\PaymentController;
 use App\Http\Controllers\Restaurants\RestaurantController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('auth')->middleware('throttle:6,1')->group(function () {
-    Route::post('request-otp', [AuthController::class, 'requestOtp']);
-    Route::post('verify-otp', [AuthController::class, 'verifyOtp']);
+Route::prefix('v1')->group(function () {
+
+    Route::prefix('auth')->middleware('throttle:6,1')->group(function () {
+        Route::post('request-otp', [AuthController::class, 'requestOtp']);
+        Route::post('verify-otp', [AuthController::class, 'verifyOtp']);
+    });
+
+    Route::middleware('auth:api')->group(function () {
+
+        Route::post('restaurant-register', [RestaurantController::class, 'register']);
+
+        Route::middleware('admin')->group(function () {
+            Route::post('users/{userId}/promote-operator', [OperatorController::class, 'promoteOperator']);
+        });
+
+        Route::middleware('operator')->prefix('restaurants')->group(function () {
+            Route::get('pending', [RestaurantApprovalController::class, 'getApprovalPendingRegister']);
+            Route::patch('{restaurant}/approve', [RestaurantApprovalController::class, 'approveRestaurant']);
+            Route::patch('{restaurant}/reject', [RestaurantApprovalController::class, 'rejectRestaurant']);
+            Route::get('{restaurant}/permit', [RestaurantController::class, 'getPermit']);
+        });
+
+        Route::middleware('restaurantOwner')->prefix('restaurants')->group(function () {
+            Route::post('{restaurant}/menu-items', [MenuController::class, 'addMenuItems']);
+            Route::put('{restaurant}/menu-items/{menuItem}', [MenuController::class, 'updateMenuItems']);
+            Route::delete('{restaurant}/menu-items/{menuItem}', [MenuController::class, 'removeMenuItems']);
+        });
+
+        Route::post('restaurants/{restaurant}/cart-items', [CartItemController::class, 'addItemToCart']);
+
+        Route::get('carts', [CartController::class, 'listCartItems']);
+        Route::put('cart-items/{cartItem}', [CartItemController::class, 'updateCartItem']);
+        Route::delete('cart-items/{cartItem}', [CartItemController::class, 'removeItemFromCart']);
+
+        Route::post('orders', [OrderController::class, 'createOrder']);
+        Route::get('orders', [OrderController::class, 'listOrder']);
+        Route::patch('orders/{order}/cancel', [OrderController::class, 'cancelOrder']);
+
+        Route::post('orders/{order}/payments', [PaymentController::class, 'sendRequestPayment']);
+        Route::post('payments/{payment}/verify', [PaymentController::class, 'verifyPayment']);
+    });
+
+    Route::get('restaurants/{restaurant}/menu', [MenuController::class, 'listMenuItems']);
 });
-
-Route::middleware('auth:api')->group(function () {
-
-    Route::post('restaurant-register', [RestaurantController::class, 'register']);
-    Route::middleware('admin')->group(function () {
-        Route::post('users/{userId}/promote-operator', [OperatorController::class, 'promoteOperator']);
-    });
-    Route::middleware('operator')->prefix('restaurants')->group(function () {
-
-        Route::get('pending', [RestaurantApprovalController::class, 'getApprovalPendingRegister']);
-        Route::patch('{restaurant}/approve', [RestaurantApprovalController::class, 'approveRestaurant']);
-        Route::patch('{restaurant}/reject', [RestaurantApprovalController::class, 'rejectRestaurant']);
-        Route::get('{restaurant}/permit', [RestaurantController::class, 'getPermit']);
-    });
-    Route::middleware('restaurantOwner')->prefix('restaurants')->group(function () {
-        Route::post('{restaurant}/add_menu_item', [MenuController::class, 'addMenuItems']);
-        Route::put('{restaurant}/update_menu_item/{menuItem}', [MenuController::class, 'updateMenuItems']);
-        Route::delete('{restaurant}/remove_menu_item/{menuItem}', [MenuController::class, 'removeMenuItems']);
-    });
-    Route::post('restaurants/{restaurant}/add_item_to_cart', [CartItemController::class, 'addItemToCart']);
-    Route::get('cart', [CartController::class, 'listCartItems']);
-    Route::put('cart/items/{cartItem}', [CartItemController::class, 'updateCartItem']);
-    Route::delete('cart/items/{cartItem}', [CartItemController::class, 'removeItemFromCart']);
-
-    Route::post('create_order', [OrderController::class, 'createOrder']);
-    Route::get('order', [OrderController::class, 'listOrder']);
-    Route::put('{order}/cancel_order', [OrderController::class, 'cancelOrder']);
-
-    Route::post('{order}/payment', [PaymentController::class, 'sendRequestPayment']);
-    Route::post('{payment}/verify_payment', [PaymentController::class, 'verifyPayment']);
-
-});
-Route::get('restaurants/{restaurant}/menu', [MenuController::class, 'listMenuItems']);
