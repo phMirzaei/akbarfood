33f7804bccd769b5da502f729314b25993a8e32a
phatemeh <phz.mirzaee@gmail.com>
2026-08-25 12:33:27 +0330

    add id for restaurant for model binding

--- app/Http/Controllers/Cart/CartItemController.php
+++ app/Http/Controllers/Cart/CartItemController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Cart\AddItemToCartRequest;
 use App\Http\Requests\Cart\UpdateCartItemRequest;
 use App\Models\Cart\CartItem;
+use App\Models\Restaurant\Restaurant;
 use App\Services\AddItemToCartService;
 use App\Services\RemoveCartItemService;
 use App\Services\UpdateCartItemService;
@@ -16,13 +17,14 @@ use Illuminate\Http\JsonResponse;
 
 class CartItemController extends Controller
 {
-    public function addItemToCart(AddItemToCartRequest $request, AddItemToCartService $addItemToCartService): JsonResponse
+    public function addItemToCart(AddItemToCartRequest $request, AddItemToCartService $addItemToCartService,Restaurant $restaurantId): JsonResponse
     {
 
         $cartItem = new AddItemToCart(
             userId: auth()->id(),
             menu_id: $request->validated('menu_id'),
             quantity: $request->validated('quantity'),
+            restaurantId: $restaurantId->id()
         );
         $addItemToCartService->execute($cartItem);
 
--- app/Http/Requests/Cart/AddItemToCartRequest.php
+++ app/Http/Requests/Cart/AddItemToCartRequest.php
@@ -19,7 +19,7 @@ class AddItemToCartRequest extends FormRequest
                 'required',
                 'integer',
                 Rule::exists('menus', 'id')->
-                    where('restaurant_id', $this->route('restaurant')),
+                    where('restaurant_id', $this->route('restaurant')->id),
             ],
             'quantity' => 'required|integer|min:1',
         ];
