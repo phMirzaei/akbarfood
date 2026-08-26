1b3bc24e5c3018fe6cc3f108fb216129641f99a4
phatemeh <phz.mirzaee@gmail.com>
2026-08-26 09:37:00 +0330

    move resource from service to controller

--- app/Services/ListOrderService.php
+++ app/Services/ListOrderService.php
@@ -3,18 +3,14 @@
 namespace App\Services;
 
 use App\DTOs\ListOrder;
-use App\Http\Resources\OrderResource;
 use App\Models\Order\Order;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ListOrderService
 {
-    public function execute(ListOrder $listOrder): AnonymousResourceCollection
+    public function execute(ListOrder $listOrder)
     {
-        return OrderResource::collection(
-            Order::with('items')
-                ->where('user_id', $listOrder->actorId)
-                ->get()
-        );
+        return Order::with('items')
+            ->where('user_id', $listOrder->actorId)
+            ->get();
     }
 }
